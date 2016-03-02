d941de39cf8f7ab73b86cc1a57e3888f4523583a
Mathew Glodack <glodack@gmail.com>
2016-03-02 13:12:57 -0800

    Create first test
    * Seems to be testing json.net and not bugsnag api

---
+++ Bugsnag.PCL.Tests/EventTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bugsnag.PCL.Request;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Bugsnag.PCL.Tests
+{
+    class EventTests
+    {
+        static JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        [Test]
+        public void Does_Event_Serialize_With_Null_Device()
+        {
+            var evt = new Event(new Exception("Test"))
+            {
+                App = new App() { ReleaseStage = "development", Version = "0.0.0" }
+            };
+
+            var json = JsonConvert.SerializeObject(evt, _settings);
+        }
+    }
+}
